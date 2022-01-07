b14b531f45ca5967eb1db36dc707ce25cfc55ea0
tabapy <tababinc2021@gmail.com>
2022-01-07 12:21:06 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+git init - .git
+git remote add name url
+git pull origin master
+git status
+git add name file
+git add.
+git add
+git commit -m "comment"
+git branch
+git branch name branch
+git checkout name branch
+git push origin master
+git reset filename
